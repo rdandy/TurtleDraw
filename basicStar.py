01dceee2a39be052f831879c326f32d58f2694e0
Andy Chiang <rdandy@gmail.com>
2024-03-19 11:05:05 +0800

    add basic star

---
+++ basicStar.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+from turtle import *
+
+color('red')
+fillcolor('yellow')
+
+begin_fill()
+
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+
+end_fill()
+
+mainloop()
